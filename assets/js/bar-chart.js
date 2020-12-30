63640650dd4c4f39d86ddf9039ef2bb6fe81b525
Callum Desktop <ctpartondev@gmail.com>
2020-12-30 23:23:39 +0000

    Added bar chart viz on profile page

---
+++ assets/js/bar-chart.js
@@ -0,0 +1,57 @@
+/**
+ * This method utilises D3.JS to create an SVG bar chart of the time the user has 
+ * spent across projects.
+ *
+ * @param {Array} data - An array of objects to create the bar chart from
+ *
+ */
+function createTimeChart(data)
+{
+    const width = 1000;
+    const height = 500;
+    const margin = { top: 50, bottom: 50, left: 25, right: 25 };
+
+    const svg = d3.select('#d3-container')
+        .append('svg')
+        .attr('width', width - margin.left - margin.right)
+        .attr('height', height - margin.top - margin.bottom)
+        .attr("viewBox", [0, 0, width, height]);
+
+    const x = d3.scaleBand()
+        .domain(d3.range(data.length))
+        .range([margin.left, width - margin.right])
+        .padding(0.1)
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data.map(x => x.time))])
+        .range([height - margin.bottom, margin.top])
+
+    svg
+        .append("g")
+        .attr("fill", '#3bbbe9')
+        .selectAll("rect")
+        .data(data.sort((a, b) => d3.descending(a.time, b.time)))
+        .join("rect")
+            .attr("x", (d, i) => x(i))
+            .attr("y", d => y(d.time))
+            .attr('title', (d) => d.time)
+            .attr("class", "rect")
+            .attr("height", d => y(0) - y(d.time))
+            .attr("width", x.bandwidth());
+
+    function yAxis(g) {
+        g.attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(y).ticks(null, data.format))
+            .attr("font-size", '20px')
+    }
+
+    function xAxis(g) {
+        g.attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+            .attr("font-size", '20px')
+    }   
+
+    svg.append("g").call(xAxis);
+    svg.append("g").call(yAxis);
+    return svg.node();
+};
--- class/pages/profile.php
+++ class/pages/profile.php
@@ -12,7 +12,7 @@
     use \Support\Context as Context;
     use \R;
 /**
- * Support /profile/
+ * Support /profile/, the profile page contains projects that the user owns
  */
     class Profile extends \Framework\Siteaction
     {
@@ -26,8 +26,25 @@
  */
         public function handle(Context $context)
         {
+            $context->web()->addCSP('script-src', 'd3js.org');
             $user =  $context->user();
-            $projects  = R::find( 'project', ' user_id = ? ', [ $user->id ] );
+            // $projects  = R::find( 'project', ' user_id = ? ', [ $user->id ] );
+            $projects = \R::findAll('project');
+            $pTime = array();
+            foreach ($projects as $p) 
+            {
+                $project = $context->load('project', (int) $p->id, TRUE);
+                $timeSpent = R::getCell('SELECT SUM(duration) FROM note WHERE project_id = :pid AND user_id = :uid',[':pid' => $project->id, 'uid' => $user->id]);
+                if (is_null($timeSpent)) 
+                {
+                    array_push($pTime, (object) array("name" => $project->name, "time" => 0));
+                } 
+                else 
+                {
+                    array_push($pTime, (object) array("name" => $project->name, "time" => $timeSpent / 86400));
+                } 
+            }
+            $context->local()->addval('pTime', $pTime);
             $context->local()->addval('projects', $projects);
             $context->local()->addval('user', $user);
             return '@content/profile.twig';
